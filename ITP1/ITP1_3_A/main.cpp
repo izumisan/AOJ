6add3125b64e6c5b45fe75f17370e887fb8c6695
izumisan <izumi.mite@gmail.com>
2019-08-26 18:38:17 +0900

    Pass: ITP1_3_A

---
+++ ITP1/ITP1_3_A/main.cpp
@@ -0,0 +1,11 @@
+// AOJ: ITP1_3_A
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_3_A&lang=jp
+#include <iostream>
+int main()
+{
+    for ( int i = 0; i < 1000; ++i )
+    {
+        std::cout << "Hello World" << std::endl;
+    }
+    return 0;
+}
