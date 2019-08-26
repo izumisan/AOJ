7796d8fb9cfb3b5cc862b67a8224fad66af7d954
izumisan <izumi.mite@gmail.com>
2019-08-27 00:53:54 +0900

    Pass: ITP1_5_C

---
+++ ITP1/ITP1_5_C/main.cpp
@@ -0,0 +1,38 @@
+// AOJ: ITP1_5_C
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_5_C&lang=jp
+#include <iostream>
+#include <string>
+
+int main()
+{
+    while ( true )
+    {
+        std::string buff {};
+        std::getline( std::cin, buff );
+        if ( buff == "0 0" )
+        {
+            break;
+        }
+
+        int h, w = 0;
+        sscanf( buff.c_str(), "%d %d", &h, &w );
+
+        for ( int i = 0; i < h; ++i )
+        {
+            for ( int j = 0; j < w; ++j )
+            {
+                if ( ( i + j ) % 2 == 0 )
+                {
+                    std::cout << "#";
+                }
+                else
+                {
+                    std::cout << ".";
+                }
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
