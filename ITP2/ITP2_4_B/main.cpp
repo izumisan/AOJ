5db3f40d1f37f997c18228003e67117ece9831a7
izumisan <izumi.mite@gmail.com>
2019-09-05 23:11:38 +0900

    Pass: ITP2_4_B

---
+++ ITP2/ITP2_4_B/main.cpp
@@ -0,0 +1,57 @@
+// AOJ: ITP2_4_B
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP2_4_B&lang=jp
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main()
+{    auto&& split = [](auto&& str)
+    {
+        std::vector<int> ret {};
+        std::string buff {};
+        std::istringstream iss { str };
+        while ( std::getline( iss, buff, ' ' ) )
+        {
+            ret.emplace_back( std::stoi( buff ) );
+        }
+        return  ret;
+    };
+
+    std::string buff {};
+    std::getline( std::cin, buff );
+    int n = std::stoi( buff );
+
+    std::getline( std::cin, buff );
+    auto&& a = split( buff );
+
+    std::getline( std::cin, buff );
+    int q = std::stoi( buff );
+
+    for ( int i = 0; i < q; ++i )
+    {
+        std::getline( std::cin, buff );
+        auto&& query = split( buff );
+        int b = query.at( 0 );
+        int m = query.at( 1 );
+        int e = query.at( 2 );
+
+        std::rotate( a.begin() + b, a.begin() + m, a.begin() + e );
+    }
+
+    for ( auto&& it = a.cbegin(); it != a.cend(); ++it )
+    {
+     if ( it != a.cbegin() )
+     {
+         std::cout << " " << *it;
+     }
+     else
+     {
+         std::cout << *it;
+     }
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
